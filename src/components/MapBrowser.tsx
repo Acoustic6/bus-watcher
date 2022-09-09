af60a1765369f147d4040f1adaa3d7c15f5b13ff
Acoustic66 <dorozhko.dmitry@gmail.com>
2022-09-09 23:52:11 +0300

    mouseover: change marker color and show text info

--- src/components/MapBrowser.tsx
+++ src/components/MapBrowser.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { VectorLayer } from 'maptalks';
+import { Marker, VectorLayer } from 'maptalks';
 import './mapBrowser.scss';
 import 'maptalks/dist/maptalks.css';
 import initMap from '../services/mapService';
-import getAllMarkers from '../services/markerService';
+import createMarker, { setMarkerColor, updateMarkerInfo } from '../services/markerService';
 import getCostMapBySiteFrom from '../services/costService';
+import sites, { Site } from '../data/sites';
+import { PURPLE } from './../constants/colors';
 
 interface MapBrowserProps {
   data: any;
@@ -14,6 +16,10 @@ interface MapBrowserProps {
 // TODO: may be keep only App component?
 class MapBrowser extends Component<MapBrowserProps> {
   map: any;
+  selectedSite: null | Site = null;
+  markersBySiteId: Map<number,Marker> = new Map<number, Marker>(); // need it?
+  hoveredOverMarkerColor = PURPLE;
+  initialMarkerColor = PURPLE;
 
   componentDidMount() {
     this.initMap();
@@ -30,42 +36,44 @@ class MapBrowser extends Component<MapBrowserProps> {
 
     const costMap = getCostMapBySiteFrom();
 
-    getAllMarkers().forEach(marker => {
+    // extract
+    sites.forEach((site: Site) => {
+      const marker = createMarker(site);
       marker.addTo(layer);
+      this.markersBySiteId.set(site.siteId, marker);
 
-      // marker.on('click', function(e) {
-      //   const color =`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-      //   e.target.updateSymbol({markerFill: color});
-      // });
-    });
-    
-    // const centerX = 37.61890;
-    // const centerY = 55.73865;
-    // const center = [centerX, centerY];
-
-    // const m = new Marker(
-    //   center,
-    //   {
-    //     'symbol' : {
-    //       'markerType': 'pin',
-    //       'markerFill': 'rgb(204, 20, 20)',
-    //       'markerFillOpacity': 1,
-    //       'markerLineColor': '#34495e',
-    //       'markerLineWidth': 3,
-    //       'markerLineOpacity': 1,
-    //       'markerLineDasharray':[],
-    //       'markerWidth': 40,
-    //       'markerHeight': 40,
-    //       'markerDx': 0,
-    //       'markerDy': 0,
-    //       'markerOpacity' : 1,
-    //     },
-    //   }).addTo(layer);
+      // extract?
+      let onMouseOverFunc = (e: any) => {
+        const marker = e.target as Marker;
+        setMarkerColor(marker, this.hoveredOverMarkerColor);
+        if (!this.isSiteSelected(site)) {
+          updateMarkerInfo(marker, site.siteId + '\n' + site.siteName, layer);
+        }
+      };
+      onMouseOverFunc = onMouseOverFunc.bind(this);
+      marker.on('mouseover', e => onMouseOverFunc(e));
       
-    // marker1.on('click', function(e) {
-    //   const color =`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-    //   e.target.updateSymbol({markerFill: color});
-    // });
+      let onMouseOutFunc = (e: any) => {
+        const marker = e.target as Marker;
+        setMarkerColor(marker, this.hoveredOverMarkerColor);
+        if (!this.isSiteSelected(site)) {
+          updateMarkerInfo(marker, site.siteId + '\n' + site.siteName, layer);
+        }
+      };
+      onMouseOutFunc = onMouseOverFunc.bind(this);
+      marker.on('mouseout', onMouseOutFunc);
+
+      marker.on('click', (e, site) => {
+        this.selectedSite = site;
+
+
+      });
+      
+    });
+  }
+
+  isSiteSelected(site: Site): boolean {
+    return site === this.selectedSite;
   }
 }
 
---
+++ src/constants/colors.ts
@@ -0,0 +1,3 @@
+export const PURPLE = '#c06ef0';
+export const LIGHT_BLUE = '#87c4f0';
+export const DARK_BLUE = '#34495e';
